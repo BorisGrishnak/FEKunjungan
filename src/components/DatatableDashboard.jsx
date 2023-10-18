6e46f029da4117bcb4f8abb0daeab6ddda01c904
Boris Grishnak <58435887+BorisGrishnak@users.noreply.github.com>
2023-10-18 16:05:23 +0700

    update1

--- src/components/DatatableDashboard.jsx
+++ src/components/DatatableDashboard.jsx
@@ -1,16 +1,41 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import DataTable, { createTheme } from 'react-data-table-component';
 import { useThemeProvider } from '../utils/ThemeContext';
 
 export default function DatatableDashboard() {
     
+const [isi, setIsi] = useState([]);
+
+useEffect(() => {
+    const fetchData = () =>{
+     axios.get('https://localhost:7225/api/Tamu').then(postData => {
+  
+     // reshaping the array
+     const customHeadings = postData.data.map(item=>({
+       "idTamu": item.idTamu,
+       "namaTamu": item.namaTamu,
+       "emailTamu": item.emailTamu,
+       "kepentingan": item.kepentingan,
+     }))
+     setIsi(customHeadings)
+    //   console.log(customHeadings);
+     })
+    }
+    fetchData()
+}, [])  
+
+const wee = isi.map((png) => png);
+// console.log(wee);
+
 const columns = [
     {
         name: 'Nama',
-        selector: row => row.namatamu,
+        selector: row => row.namaTamu,
     },
     {
         name: 'Email',
-        selector: row => row.emailtamu,
+        selector: row => row.emailTamu,
     },
     {
         name: 'Kepentingan',
@@ -20,33 +45,6 @@ const columns = [
 
 const { currentTheme } = useThemeProvider();
 
-const data = [
-    {
-        id: 1,
-        namatamu: 'Jalal',
-        emailtamu: 'jalal@gmail.com',
-        kepentingan: 'Meeting',
-    },
-    {
-        id: 2,
-        namatamu: 'Darmaji',
-        emailtamu: 'darmaji@gmail.com',
-        kepentingan: 'Meeting',
-    },
-    {
-        id: 3,
-        namatamu: 'Suroto',
-        emailtamu: 'suroto@gmail.com',
-        kepentingan: 'Meeting',
-    },
-    {
-        id: 4,
-        namatamu: 'Togik',
-        emailtamu: 'togik@gmail.com',
-        kepentingan: 'Meeting',
-    },
-]
-
     return (
         <DataTable
             pagination
@@ -54,7 +52,7 @@ const data = [
             fixedHeaderScrollHeight="300px"
             responsive
             columns={columns}
-            data={data}
+            data={wee}
             theme={currentTheme}
         />
     );
